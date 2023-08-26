8370a8051371c3af68c2d50fa4a76cc5e0f6468f
Patrick He <77468352+he-patrick@users.noreply.github.com>
2023-08-26 16:52:27 -0400

    Connect php and python

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Predator Detector</title>
+</head>
+<body>
+    <form method="post">
+        <label for="user_input">Enter a word:</label>
+        <input type="text" name="user_input">
+        <button type="submit">Submit</button>
+    </form>
+    <?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $user_input = $_POST["user_input"];
+        $output = shell_exec("python text.py $user_input");
+        echo "<p>Modified input: $output</p>";
+    }
+    ?>
+</body>
+</html>
