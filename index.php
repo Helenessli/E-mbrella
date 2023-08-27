b1040a2010bed488a1c4abec13f3540cf0c7c78c
Jerry Zhao <jerryzhao@Jerrys-MacBook-Air.local>
2023-08-27 00:10:22 -0400

    new

--- index.php
+++ index.php
@@ -2,19 +2,31 @@
 <html>
 <head>
     <title>Predator Detector</title>
+    <link rel="stylesheet" type="text/css" href="styles.css">
 </head>
 <body>
-    <form method="post">
-        <label for="user_input">Enter a word:</label>
-        <input type="text" name="user_input">
-        <button type="submit">Submit</button>
-    </form>
-    <?php
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $user_input = $_POST["user_input"];
-        $output = shell_exec("python text.py $user_input");
-        echo "<p>Result: $output</p>";
-    }
-    ?>
+    <div class="container">
+        <h1>E-mbrella Predator Detector</h1>
+        <form method="post">
+            <label for="user_input">Enter a sentence:</label>
+            <input type="text" name="user_input">
+            <button type="submit">Submit</button>
+        </form>
+        <div class="result">
+            <?php
+            if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                $user_input = $_POST["user_input"];
+                $output = shell_exec("python text.py $user_input");
+                $prediction = $output[1];
+                $confidence_score = substr($output, 3, 7);
+                if ($prediction == 1){
+                    $pred_result = "Predator";
+                }else{$pred_result = "Normal";}
+                echo "<p>Result: $pred_result</p>";
+                echo "<p>Confidence level: $confidence_score</p>";
+            }
+            ?>
+        </div>
+    </div>
 </body>
 </html>
--- text.py
+++ text.py
@@ -4,15 +4,12 @@ from BERT import predict_text
 
 
 if __name__ == "__main__":
-    # if len(sys.argv) != 2:
-    #     print("Usage: python text.py <input>")
-    # else:
 
     user_input = sys.argv[1]
+
+    # print(user_input)
     
     # Run ML code here
     arr = predict_text(user_input)
 
-    print(str(arr[0])+' '+str(arr[1]))
-                    
-            
+    print(arr)
