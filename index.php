b86b5bb0d3e8e5d1877ca95b19b65b3d4e9c0269
helenessli <helenessli6@gmail.com>
2023-08-27 00:09:28 -0400

    add color for ui

--- index.php
+++ index.php
@@ -2,14 +2,19 @@
 <html>
 <head>
     <title>Predator Detector</title>
+    <link rel="stylesheet" type="text/css" href="app.css">
 </head>
-<body>
+<header class="header">
+    <h1 class="h1">E-mbrella Detector</h1>
+</header>
+<body class="body">
     <form method="post">
-        <label for="user_input">Enter a word:</label>
+        <label for="user_input">Enter a phrase:</label>
         <input type="text" name="user_input">
         <button type="submit">Submit</button>
     </form>
     <?php
+
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $user_input = $_POST["user_input"];
         $output = shell_exec("python text.py $user_input");
