1b25f47c27c8d28b98b80b3fa590305231213cd6
Kazame <76440010+Jerry-YZhao@users.noreply.github.com>
2023-08-27 10:15:23 -0400

    Add files via upload

--- index.php
+++ index.php
@@ -11,38 +11,30 @@
         <h1 class="h1">E-mbrella Predator Detector</h1>
     </div>
     <div class="body">
-        <div class="formcontainer">
         <form class="form" method="post">
-            <label for="user_input" class="label">Enter a message:</label><br>
-            <input type="text" name="user_input"><br>
-            <button type="submit">Submit</button>
+            <label for="user_input">Enter a sentence: </label>
+            <input type="text" name="user_input">
+            <button type="submit" name="submit">Submit</button>
         </form>
-        </div>
         <div class="result">
             <?php
             if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 $user_input = $_POST["user_input"];
                 $output = shell_exec("python text.py $user_input");
                 $prediction = $output[1];
-                $confidence_score = substr($output, 3, 7);
-                $resultStyle = ""; // Initialize style variable
-
+                $confidence_score = substr($output, 3, 5);
                 if ($prediction == 1) {
-                    if ($confidence_score > 0.60) {
-                        $resultStyle = "color: red;";
-                    } else {
-                        $resultStyle = "color: orange;";
-                    }
-                    $pred_result = "Predator";
+                    if ($confidence_score >= 0.6){
+                        $pred_result = "Predator";
+                        $text_color = "red";
+                    }else{$pred_result = "Might be a Predator"; $text_color = "orange";}
                 } else {
-                    $resultStyle = "color: green;";
                     $pred_result = "Normal";
+                    $text_color = "green";
                 }
-            
-                echo "<p class=\"message\">Message: $user_input</p>";
-                echo "<p class=\"result\" style=\"$resultStyle\">Result: $pred_result</p>";
-                echo "<p class=\"confidence\">Confidence level: $confidence_score</p>";
-            
+                echo "<p> Message: $user_input </p>";
+                echo "<p style='color: $text_color;'> Result: $pred_result </p>";
+                echo "<p> Confidence level: $confidence_score </p>";
             }
             ?>
         </div>
